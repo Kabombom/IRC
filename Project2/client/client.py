792156b45b78c5cc7be51731c6af661ac9c3fd2a
Miguel Machado <mantonio@student.dei.uc.pt>
2015-12-09 22:18:04 +0000

    Added methods

--- Project2/client/client.py
+++ Project2/client/client.py
@@ -1,5 +1,117 @@
-from socket import *
+import socket
+import sys
+import json
+import os
 
 
-def client(port):
-    client_socket = client_socket.
+def read_file(filename):
+    file = open(filename, "r")
+    file_lines = []
+    for line in file:
+        file_list = line.split(',')
+        file_lines.append(file_list)
+    return file_lines
+
+
+def write_file(filename, lines):
+    file = open(filename, "w")
+    file_lines = read_file(filename)
+    for list in file_lines:
+        file.write(list)
+
+
+def upload(filename):
+    lines = read_file(filename)
+    return lines
+
+
+def filename_validation(filename):
+    if not os.path.isfile(filename):
+        return False
+    return True
+
+
+def filename_input():
+    filename = input("Filename: ")
+    while not filename_validation(filename):
+        filename = input("Invalid input, filename: ")
+    return filename
+
+
+def options_validation(option):
+    try:
+        choice = int(option)
+        if choice < 0 or choice > 5:
+            return False
+        return True
+    except ValueError:
+        print("Invalid input")
+        return False
+
+
+def menu():
+    option = input("[0]--> Register\n"
+                   "[1]--> Login\n"
+                   "[2]--> List\n"
+                   "[3]--> Upload\n"
+                   "[4]--> Download\n"
+                   "[5]--> Exit\n"
+                   "What do you wish to do: ")
+    while not options_validation(option):
+        option = input("Invalid input\n"
+                       "[0]--> Register\n"
+                       "[1]--> Login\n"
+                       "[2]--> List\n"
+                       "[3]--> Upload\n"
+                       "[4]--> Download\n"
+                       "[5]--> Exit\n"
+                       "What do you wish to do: ")
+    return int(option)
+
+
+def client(port, data):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Socket creation sucefull")
+    host = socket.gethostname()
+    try:
+        client_socket.connect((host, port))
+    except socket.error as e:
+        print('Unable to connect, exception type %s' % e)
+        sys.exit()
+
+    if isinstance(data, int):
+        data_string = str(data)
+        client_socket.send(data_string.encode('utf-8'))
+    if isinstance(data, list) or isinstance(data, dict):
+        data_string = json.dumps(data).encode('utf-8')
+        client_socket.send(data_string)
+
+
+def main(client_socket):
+    port = 9000
+    while True:
+        choice = menu()
+        if choice == 0:
+            client(port, 0)
+        elif choice == 1:
+            client(port, 1)
+        elif choice == 2:
+            client(port, 2)
+        elif choice == 3:
+            filename = filename_input()
+            lines = upload(filename)
+            to_send = (filename, lines)
+            client(port, to_send)
+        elif choice == 4:
+            client(port, 4)
+            request = client_socket.recv(1024)
+            request = request.decode('utf-8')
+            request = json.loads(request)
+            write_file()
+        elif choice == 5:
+            return
+
+
+if __name__ == '__main__':
+    main()
--- Project2/server/server.py
+++ Project2/server/server.py
@@ -1,12 +1,35 @@
-from socket import *
-import _thread
+import socket
+import os
+import json
 
 
-def create_user(users):
+def confirm_login(filename, login_credentials):
+    users = read_file(filename)
+    for user in users:
+        if login_credentials[0] == user[0] and login_credentials[1] == user[1]:
+            return True
+    return False
+
+
+def login(filename):
+    name = input("Username: ")
+    pwd = input("Password: ")
+    user = [name, pwd]
+    while not confirm_login(filename, user):
+        print("Invalid input")
+        name = input("Username: ")
+        pwd = input("Password: ")
+        user = [name, pwd]
+    return [name, pwd]
+
+
+def register(filename):
     name = input("Username: ")
     password = input("Password: ")
     new_user = [name, password]
-    users.append(new_user)
+    file_lines = read_file(filename)
+    file_lines.append(new_user)
+    write_file(filename, file_lines)
 
 
 def remove_user(users):
@@ -15,38 +38,60 @@ def remove_user(users):
     for i in range(len(users)):
         if users[i][0] == name and users[i][1] == pwd:
             users.remove(i)
-            return
 
 
-def write_file(filename, dicio):
+def write_file(filename):
     file = open(filename, "w")
+    file_lines = read_file(filename)
+    for line in file_lines:
+        file.write(line)
 
 
 def read_file(filename):
     file = open(filename, "r")
+    file_lines = []
     for line in file:
-        print(line)
+        file_list = line.split(',')
+        file_lines.append(file_list)
+    return file_lines
 
 
-# function that takes care of CTRL+C signal
-def signal_handler(signal, frame):
-    print(' pressed...exiting now')
+def list():
+    os.listdir('')
 
 
-def client_handler(client, address):
-    while True:
-        print("Connection by: ", address)
-        request = client.recv(1024)
-        request = request.decode()
-        if not request:
-            break
-        client.send("Cena")
+def upload(filename, file_lines):
+    file = open(filename, "w")
+    for line in file_lines:
+        file.write(line)
+
+
+def client_handler(client, request):
+    if isinstance(request, int):
+        if int(request) == 0:
+            register("users.txt")
+        elif int(request) == 1:
+            login("users.txt")
+        elif int(request) == 2:
+            list()
+        elif int(request) == 4:
+            filename = input("Filename: ")
+            data = read_file(filename)
+            data_string = json.dumps(data).encode('utf-8')
+            client.send(data_string)
+        elif int(request) == 5:
+            return
+
+    if isinstance(request, tuple):
+        filename = str(request[0])
+        lines = request[1]
+        upload(filename, lines)
 
 
 def server(port):
     # Create an INET, streaming socket, afterwards set socket options, bind it to an adresss and start listening
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('', port))
     server_socket.listen(5)
     print("The server is ready to receive")
@@ -54,11 +99,17 @@ def server(port):
     while True:
         # Establish connection to client
         client, address = server_socket.accept()
-        _thread.start_new_thread(client_handler, (client, address))
+        print("Connection by: ", address)
+        request = client.recv(1024)
+        request = request.decode("utf-8")
+        request = json.loads(request)
+
+        client_handler(client, request)
 
 
 def main():
     server(9000)
+
 
 if __name__ == '__main__':
     main()
